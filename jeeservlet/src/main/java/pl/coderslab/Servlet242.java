07d51d4fc0563c914576fb082d94f9e27aa7343f
Janek <zlaasu@gmail.com>
2019-10-01 20:58:15 +0200

    initial

---
+++ jeeservlet/src/main/java/pl/coderslab/Servlet242.java
@@ -0,0 +1,19 @@
+package pl.coderslab;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "Servlet242", urlPatterns = {"/Servlet242"})
+public class Servlet242 extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
